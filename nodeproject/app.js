b84c73eb87df40f2dfa3bea07e23e20d5a6f0978
berthobox <letamemail@gmail.com>
2017-08-25 18:58:15 +0000

    \start with app

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,16 @@
-var ciao = "ciao variables";
-console.log(ciao);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT; // può essere impostato anche un numero Es. 5000
+
+app.get('/', function(req, res){
+    res.send('Hola risposta!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hola routing!');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
